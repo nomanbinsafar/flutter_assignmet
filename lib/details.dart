08d310d05576e2fb91b7cbe8e53c730be8f6f8f1
nomanbinsafar <nomanbinsafar@gmail.com>
2023-11-30 20:57:03 +0600

    second Page initiate

---
+++ lib/details.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+class BookDetailsPage extends StatelessWidget {
+  final Book book;
+
+  const BookDetailsPage({Key? key, required this.book}) : super(key: key);
+
+  
